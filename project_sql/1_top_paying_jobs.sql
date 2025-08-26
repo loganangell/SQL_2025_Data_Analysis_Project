71492c907557b4cda9fe760347f2ec519a568303
Logan Angell <loganangell@Outlook.com>
2025-08-26 00:07:45 -0400

    Commit-1_Top_Paying_Jobs

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,31 @@
+/*
+What is the top paying job available (data analyst, data scientist, business analyst, and data engineer)?
+
+1. Return the top 10 highest-paying jobs based on the job titles mentioned above
+    a. Can also return a seperate search of jobs available remotely
+    b. Focus on job postings that are from 2025
+2. Focus on job postings that specifies salaries (remove null values)
+3. Focus on highlighting top-paying opportunities in fields I have an interest in
+*/
+
+SELECT
+    job_id,
+    name AS company_name,
+    job_title_short AS position,
+    job_title AS full_job_title,
+    job_location
+    job_posted_date,
+    salary_year_avg AS average_yearly_salary,
+    -- job_work_from_home, -- this column is for when we want to filter for remote jobs
+    job_posted_date
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short IN ('Data Analyst', 'Data Scientist', 'Business Analyst', 'Data Engineer') AND --filter if opportunities should be split up
+    salary_year_avg IS NOT NULL AND
+    job_posted_date >= '2025-01-01'
+    -- AND job_location = 'Anywhere' -- uncomment this line for remote jobs
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10;
